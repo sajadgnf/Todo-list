68b4ed7e99af8511f6e7d26342a918cee3ad04c1
iamsajadgnf <sajadganjifar@gmail.com>
2021-09-20 18:15:44 +0430

    completedTodoList

---
+++ assets/js/script.js
@@ -0,0 +1,139 @@
+let $ = document
+
+const inputTodo = $.querySelector("#todo_input")
+const addTodoBtn = $.querySelector(".add_btn")
+const removeTodoBtn = $.querySelector(".remove_btn")
+const todoBox = $.querySelector(".todo")
+const doneBox = $.querySelector(".done")
+const errorText = $.querySelector(".error")
+var todoList = []
+var doneList = []
+
+// Empty Frame Error
+const error = item => {
+    if (item) {
+        return true
+    }
+    errorText.innerHTML = "Please Fill The Frame First"
+    return false
+}
+
+// Clear Error
+const clearError = () => {
+    errorText.innerHTML = ""
+}
+
+// Bild Todo
+const bildTodo = () => {
+    todoBox.innerHTML = ""
+    doneBox.innerHTML = ""
+
+    // Active Project
+    todoList.forEach(item => {
+        // Todo Item
+        let todoItem = $.createElement("div")
+        todoItem.setAttribute("class", "todo_item")
+
+        // Todo Text
+        let todoText = $.createElement("h3")
+        todoText.innerHTML = item
+
+        // Tick Icon
+        let todoTick = $.createElement("i")
+        todoTick.classList.add("todo_tick")
+        todoTick.addEventListener("click", event => {
+            tickDoneProject(todoItem, event)
+        })
+
+        todoItem.append(todoText, todoTick)
+        todoBox.appendChild(todoItem)
+    })
+
+    // doneProject
+    doneList.forEach(element => {
+        // Todo Item
+        let todoItem = $.createElement("div")
+        todoItem.setAttribute("class", "todo_item")
+
+        // Todo Text
+        let todoText = $.createElement("h3")
+        todoText.innerHTML = element
+        todoText.style.textDecoration = "line-through"
+        todoText.style.color = "#444"
+
+        // Tick Icon
+        let todoTick = $.createElement("i")
+        todoTick.classList.add("fa", "fa-check")
+        todoTick.addEventListener("click", event => {
+            tickDoneProject(todoItem, event)
+        })
+
+        todoItem.append(todoText, todoTick)
+        doneBox.append(todoItem)
+    })
+}
+
+// Add Todo
+const addTodo = event => {
+    event.preventDefault()
+
+    if (!error(inputTodo.value)) {
+        return false
+    }
+
+    todoList.push(inputTodo.value)
+    localStorage.setItem("todoList", JSON.stringify(todoList))
+
+    bildTodo()
+
+    inputTodo.value = ""
+}
+
+// Tick Done Project
+const tickDoneProject = (item, event, index) => {
+    if (event.target.className == "todo_tick") {
+        doneList.push(item.firstElementChild.innerHTML)
+        todoList.splice(index, 1)
+    }
+
+    else {
+        todoList.push(item.firstElementChild.innerHTML)
+        doneList.splice(index, 1)
+    }
+
+    localStorage.setItem("doneList", JSON.stringify(doneList))
+    localStorage.setItem("todoList", JSON.stringify(todoList))
+    fetchTodoList()
+}
+
+// Remove Todo
+const removeTodo = event => {
+    let confirmDeleteList = confirm("Are You Sure To Delete ?")
+    if (confirmDeleteList) {
+        event.preventDefault()
+        doneList.length = 0
+    }
+    localStorage.setItem("doneList", JSON.stringify(doneList))
+    fetchTodoList()
+}
+
+// fetchTodoList
+const fetchTodoList = () => {
+    if (localStorage.getItem("todoList") && localStorage.getItem("doneList")) {
+        doneList = JSON.parse(localStorage.getItem("doneList"))
+        todoList = JSON.parse(localStorage.getItem("todoList"))
+    }
+    else {
+        todoList = []
+        doneList = []
+        localStorage.setItem("todoList", JSON.stringify(todoList))
+        localStorage.setItem("doneList", JSON.stringify(doneList))
+    }
+    bildTodo()
+}
+
+// Events
+addTodoBtn.addEventListener("click", addTodo)
+inputTodo.addEventListener("focus", clearError)
+removeTodoBtn.addEventListener("click", removeTodo)
+fetchTodoList()
